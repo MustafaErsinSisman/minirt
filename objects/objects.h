402feeb09e88415d1209811b1670d41a58d8e7d1
Mustafa Ersin Şişman <sismanmustafaersin@gmail.com>
2025-11-26 18:18:31 +0300

    ray color methodu detaylandırıldı ray.c dosyası detaylandırıldı

--- objects/objects.h
+++ objects/objects.h
@@ -61,7 +61,7 @@ typedef struct s_cylinder
 	t_vector3	rgb;
 }	t_cylinder;
 
-typedef union u_objects
+typedef union u_objects // * tüm nesne türlerini içeren union kullanıldı ki tek bir yapı altında tüm nesne türlerine erişilebilsin ve bellek tasarrufu sağlansın
 {
 	t_ambient	ambiant;
 	t_camera	camera;
@@ -71,7 +71,7 @@ typedef union u_objects
 	t_cylinder	cylinder;
 }	t_objects;
 
-typedef enum e_type
+typedef enum e_type // * nesne türü enum kullanıldı ki her bir nesne türü için sabit bir değer atansın ve kodun okunabilirliği artsın bellek tasarrufu da sağlar
 {
 	AMBIANT,
 	CAMERA,
@@ -83,45 +83,47 @@ typedef enum e_type
 
 typedef struct s_ray
 {
-	t_vector3	origin;
-	t_vector3	direction;
+	t_vector3	origin; // * ray origin noktası 
+	t_vector3	direction; // * ray direction vektörü 
 }	t_ray;
 
-typedef struct s_hit_record
+typedef struct s_hit_record // * çarpma kaydı
 {
-	t_vector3	p;
-	t_vector3	normal;
-	double		t;
-	// t_material	*material;
+	t_vector3	p; // * çarpma noktası
+	t_vector3	normal; // * yüzey normali
+	double		t; // * t değeri çarpma noktasına olan uzaklık
+	// t_material	*material; // * malzeme bilgisi çarpılan yüzeyin malzemesi olabilir ileride eklenebilir
 }	t_hit_record;
 
-typedef struct s_hit_status
+typedef struct s_hit_status // * çarpma durumu
 {
-	const t_ray		*ray;
-	double			t_min;
-	double			t_max;
-	t_hit_record	*rec;
+	const t_ray		*ray; // * çarpan ışın
+	double			t_min; // * minimum t değeri bu değerin altındaki çarpmalar dikkate alınmaz
+	double			t_max; // * maximum t değeri bu değerin üzerindeki çarpmalar dikkate alınmaz
+	t_hit_record	*rec; // * çarpma kaydı bu yapıya çarpma bilgileri yazılır ve bu bilgiler render fonksiyonunda kullanılır
 }	t_hit_status;
 
 typedef bool (*t_hit_func)(struct s_object *object,
-	t_hit_status *status);
+	t_hit_status *status); // * çarpma fonksiyonu bu fonksiyon her bir nesne türü için farklı olacak çünkü her bir nesnenin çarpma testi farklıdır pointer to function ile tanımlandı ki her bir nesne türü için farklı fonksiyonlar atanabilsin
 
-typedef struct s_object // * render için objeler
+
+
+typedef struct s_object // * render için objeler sadece obje türü ve verisi
 {
-	t_type		type;
-	void		*data;
-	t_hit_func	hit;
+	t_type		type; // * nesne türü
+	void		*data; // * nesne verisi
+	t_hit_func	hit; // * çarpma fonksiyonu
 }	t_object;
 
-typedef struct s_obje_list // * parser için objeler
+typedef struct s_obje_list // * parser için objeler liste yapısı
 {
-	t_type				type;
-	t_objects			objects;
-	struct s_obje_list	*next;
+	t_type				type; // * nesne türü
+	t_objects			objects; // * nesne verisi
+	struct s_obje_list	*next; // * sonraki nesne
 }	t_obje_list;
 
-bool		hit_world(t_list *world, t_hit_status *status);
-t_object	*new_sphere(t_vector3 center, double diameter);
-bool		hit_sphere(t_object *object, t_hit_status *status);
+bool		hit_world(t_list *world, t_hit_status *status); // * dünyadaki nesnelere çarpma testi fonksiyonu
+t_object	*new_sphere(t_vector3 center, double diameter); // * yeni küre nesnesi oluşturma fonksiyonu
+bool		hit_sphere(t_object *object, t_hit_status *status); // * küre çarpma testi fonksiyonu
 
 #endif
--- objects/world.c
+++ objects/world.c
@@ -14,28 +14,28 @@
 
 bool	hit_world(t_list *world, t_hit_status *status)
 {
-	t_hit_record	temp_rec;
-	bool			hit_anything;
-	double			closest_so_far;
-	t_list			*current_node;
-	t_hit_status	temp_status;
+	t_hit_record	temp_rec; // * geçici çarpma kaydı 
+	bool			hit_anything; // * herhangi bir şeye çarpma durumu
+	double			closest_so_far; // * şimdiye kadar bulunan en yakın çarpma noktası
+	t_list			*current_node; // * dünyadaki mevcut nesne düğümü
+	t_hit_status	temp_status; // * geçici çarpma durumu
 
-	hit_anything = false;
-	closest_so_far = status->t_max;
-	current_node = world;
-	temp_status = *status;
-	temp_status.rec = &temp_rec;
-	while (current_node)
+	hit_anything = false; // * başlangıçta herhangi bir şeye çarpmadı
+	closest_so_far = status->t_max; // * başlangıçta en yakın çarpma noktası maksimum t değeri
+	current_node = world; // * dünyadaki ilk nesne düğümü
+	temp_status = *status; // * geçici çarpma durumu kopyalandı
+	temp_status.rec = &temp_rec; // * geçici çarpma kaydı atandı
+	while (current_node) // * dünyadaki tüm nesneler için çarpma testi yapıldı
 	{
-		temp_status.t_max = closest_so_far;
+		temp_status.t_max = closest_so_far; // * şimdiye kadar bulunan en yakın çarpma noktasına güncellendi
 		if (((t_object *)current_node->content)->hit(current_node->content,
-				&temp_status))
+				&temp_status)) // * mevcut nesneye çarpma testi yapıldı
 		{
-			hit_anything = true;
-			closest_so_far = temp_rec.t;
-			*(status->rec) = temp_rec;
+			hit_anything = true; // * herhangi bir şeye çarptı
+			closest_so_far = temp_rec.t; // * en yakın çarpma noktası güncellendi
+			*(status->rec) = temp_rec; // * çarpma kaydı güncellendi
 		}
-		current_node = current_node->next;
+		current_node = current_node->next; // * sonraki nesne düğümüne geçildi
 	}
-	return (hit_anything);
+	return (hit_anything); // * herhangi bir şeye çarpma durumu döndürüldü
 }
